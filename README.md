1bcb94b9984bebbbb86f7485cbb326631a7fd27c
Slick <yoru.omg@gmail.com>
2018-04-04 17:13:14 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,44 @@
+﻿# VolumeChanger
+
+One Paragraph of project description goes here
+
+## Getting Started
+
+These instructions will get you a copy of the project up and running on your local machine for development and testing purposes. See deployment for notes on how to deploy the project on a live system.
+
+### Prerequisites
+
+What things you need to install the software and how to install them
+
+```
+Give examples
+```
+
+### Installing
+
+A step by step series of examples that tell you have to get a development env running
+
+Say what the step will be
+
+```
+Give the example
+```
+
+And repeat
+
+```
+until finished
+```
+
+End with an example of getting some data out of the system or using it for a little demo
+
+## Running the tests
+
+Explain how to run the automated tests for this system
+
+
+## Acknowledgments
+
+* Hat tip to anyone who's code was used
+* Inspiration
+* etc
---
+++ Volume/Program.cs
@@ -0,0 +1,33 @@
+﻿using AudioSwitcher.AudioApi.CoreAudio;
+using System;
+
+namespace SetVolume
+{
+    internal class VolumeSetter
+    {
+        private static CoreAudioDevice device = new CoreAudioController().DefaultPlaybackDevice;
+
+        private static void Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                Console.WriteLine($"Current volume is {device.Volume}");
+                return;
+            }
+            else
+            {
+                double newVolume = 0;
+                if (Double.TryParse(args[0], out newVolume))
+                {
+                    double previousVolume = device.Volume;
+                    device.Volume = newVolume;
+                    Console.WriteLine($"Volume was changed from {previousVolume} to {device.Volume}");
+                }
+                else
+                {
+                    Console.WriteLine($"Volume ({device.Volume}) wasn't changed because '{args[0]}' could not be parsed into a number.");
+                }
+            }
+        }
+    }
+}
