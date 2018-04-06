9a8ecec6024c2d913f94f3526b9281835b19738e
Slick <yoru.omg@gmail.com>
2018-04-07 00:01:06 +0200

    help command

--- Volume/Program.cs
+++ Volume/Program.cs
@@ -1,32 +1,38 @@
 ﻿using AudioSwitcher.AudioApi.CoreAudio;
 using System;
+using System.Linq;
 
-namespace SetVolume
+namespace VolumeChanger
 {
-    internal class VolumeSetter
+    internal class VolumeChanger
     {
         private static CoreAudioDevice device = new CoreAudioController().DefaultPlaybackDevice;
+        private static string[] HelpKeywords = { "-h", "--h", "help", "-help", "--help" };
 
         private static void Main(string[] args)
         {
-            if (args.Length == 0)
+            if (args.Length <= 0)
             {
                 Console.WriteLine($"Current volume is {device.Volume}");
                 return;
             }
+
+            if (HelpKeywords.Select(k => k.ToLower()).Any(k => k == args[0]))
+            {
+                Console.WriteLine("usage: volume [--help] <number>");
+                return;
+            }
+
+            double newVolume = 0;
+            if (Double.TryParse(args[0], out newVolume))
+            {
+                double previousVolume = device.Volume;
+                device.Volume = newVolume;
+                Console.WriteLine($"Volume was changed from {previousVolume} to {device.Volume}");
+            }
             else
             {
-                double newVolume = 0;
-                if (Double.TryParse(args[0], out newVolume))
-                {
-                    double previousVolume = device.Volume;
-                    device.Volume = newVolume;
-                    Console.WriteLine($"Volume was changed from {previousVolume} to {device.Volume}");
-                }
-                else
-                {
-                    Console.WriteLine($"Volume ({device.Volume}) wasn't changed because '{args[0]}' could not be parsed into a number.");
-                }
+                Console.WriteLine($"Volume ({device.Volume}) wasn't changed because '{args[0]}' could not be parsed into a number.");
             }
         }
     }
